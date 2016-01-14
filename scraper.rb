227cf2a468e54e12ce5749966477374f894596b1
Tony Bowden <tony@tmtm.com>
2016-01-14 15:07:13 +0000

    Less verbosity

--- scraper.rb
+++ scraper.rb
@@ -6,7 +6,7 @@ require 'wikidata/fetcher'
 
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/turkey-tbmm-wikipedia', column: 'wikipedia__tk')
 names.each_slice(1000) do |sliced|
-  EveryPolitician::Wikidata.scrape_wikidata(names: { tr: sliced }, output: true)
+  EveryPolitician::Wikidata.scrape_wikidata(names: { tr: sliced })
 end
 warn EveryPolitician::Wikidata.notify_rebuilder
 
