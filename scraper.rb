f8725ffd0752dfafe08853b5f621fbe370985de2
Tony Bowden <tony@tmtm.com>
2016-02-07 11:15:46 +0000

    Slice in 500s

--- scraper.rb
+++ scraper.rb
@@ -5,7 +5,7 @@ require 'pry'
 require 'wikidata/fetcher'
 
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/turkey-tbmm-wikipedia', column: 'wikipedia__tk')
-names.each_slice(1000) do |sliced|
+names.each_slice(500) do |sliced|
   EveryPolitician::Wikidata.scrape_wikidata(names: { tr: sliced })
 end
 warn EveryPolitician::Wikidata.notify_rebuilder
