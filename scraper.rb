a725722704e90b50fb23a2f2bacb7ab2b80a89d7
Tony Bowden <tony@tmtm.com>
2016-02-28 07:36:28 +0000

    Shuffle results (in case die in the middle)

--- scraper.rb
+++ scraper.rb
@@ -5,7 +5,7 @@ require 'pry'
 require 'wikidata/fetcher'
 
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/turkey-tbmm-wikipedia', column: 'wikipedia__tk')
-names.each_slice(500) do |sliced|
+names.shuffle.each_slice(250) do |sliced|
   EveryPolitician::Wikidata.scrape_wikidata(names: { tr: sliced })
 end
 warn EveryPolitician::Wikidata.notify_rebuilder
