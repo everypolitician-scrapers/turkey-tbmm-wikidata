72ca4b4b1da93f1b0c434f33e21a2846ba58c9c6
Tony Bowden <tony@tmtm.com>
2016-03-05 17:56:58 +0000

    Fetch 250 at a time

--- scraper.rb
+++ scraper.rb
@@ -4,7 +4,7 @@
 require 'wikidata/fetcher'
 
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/turkey-tbmm-wikipedia', column: 'wikipedia__tk')
-names.shuffle.each_slice(100) do |sliced|
+names.shuffle.each_slice(250) do |sliced|
   EveryPolitician::Wikidata.scrape_wikidata(names: { tr: sliced })
 end
 
