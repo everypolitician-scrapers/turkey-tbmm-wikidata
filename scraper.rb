70d777ff9aadaaddf6e17941dbd8be07e5f9fddb
Tony Bowden <tony@tmtm.com>
2016-03-05 17:55:29 +0000

    Use webhook instead of notifier

--- scraper.rb
+++ scraper.rb
@@ -1,12 +1,10 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'pry'
 require 'wikidata/fetcher'
 
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/turkey-tbmm-wikipedia', column: 'wikipedia__tk')
-names.shuffle.each_slice(250) do |sliced|
+names.shuffle.each_slice(100) do |sliced|
   EveryPolitician::Wikidata.scrape_wikidata(names: { tr: sliced })
 end
-warn EveryPolitician::Wikidata.notify_rebuilder
 
