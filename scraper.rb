1a7db6c58a9c464736b514c2a082f23b956fda6a
Tony Bowden <tony@tmtm.com>
2016-11-14 13:43:21 +0000

    Scrape any IDs we already know
    This way we shouldn't lose any IDs that are already reconciled, at least
    until after we re-reconile them to someone else.

--- scraper.rb
+++ scraper.rb
@@ -1,8 +1,10 @@
 #!/bin/env ruby
 # encoding: utf-8
 
+require 'everypolitician'
 require 'wikidata/fetcher'
 
+existing = EveryPolitician::Index.new.country("Turkey").legislature('Assembly').popolo.persons.map(&:wikidata).compact
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/turkey-tbmm-wikipedia', column: 'wikipedia__tk')
-EveryPolitician::Wikidata.scrape_wikidata(names: { tr: names })
 
+EveryPolitician::Wikidata.scrape_wikidata(ids: existing, names: { tr: names })
