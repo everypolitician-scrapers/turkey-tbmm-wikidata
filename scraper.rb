a4ad2218e33a638501d1a1239afa63e6dd418703
Tony Bowden <tony@tmtm.com>
2016-12-24 07:55:33 +0000

    Read correct column of other Morph scraper
    We fix that from wikipedia__tk to wikipedia__tr

--- scraper.rb
+++ scraper.rb
@@ -5,6 +5,6 @@ require 'everypolitician'
 require 'wikidata/fetcher'
 
 existing = EveryPolitician::Index.new.country("Turkey").legislature('Assembly').popolo.persons.map(&:wikidata).compact
-names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/turkey-tbmm-wikipedia', column: 'wikipedia__tk')
+names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/turkey-tbmm-wikipedia', column: 'wikipedia__tr')
 
 EveryPolitician::Wikidata.scrape_wikidata(ids: existing, names: { tr: names })
