d8301ade3b1df62662e9385165c039103bc6be26
Tony Bowden <tony@tmtm.com>
2017-09-12 07:22:09 +0100

    use scraper's `wikidata` column
    The main scraper now also has a wikidata column, so we can use that
    directly.

--- scraper.rb
+++ scraper.rb
@@ -5,6 +5,9 @@ require 'everypolitician'
 require 'wikidata/fetcher'
 
 existing = EveryPolitician::Index.new.country("Turkey").legislature('Assembly').popolo.persons.map(&:wikidata).compact
-names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/turkey-tbmm-wikipedia', column: 'wikipedia__tr')
+scraper_ids = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/turkey-tbmm-wikipedia', column: 'wikidata')
 
-EveryPolitician::Wikidata.scrape_wikidata(ids: existing, names: { tr: names })
+# This _should_ be unnecessary, but we can remove it once we've switched to all IDs
+scraper_names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/turkey-tbmm-wikipedia', column: 'wikipedia__tr')
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: existing | scraper_ids, names: { tr: scraper_names })
